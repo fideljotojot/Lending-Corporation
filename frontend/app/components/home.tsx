2f63d12f179aa7591900be18ff0c977c9ce5a557
fideljotojot <fideljotojot@gmail.com>
2026-03-27 11:36:22 +0800

    feat: enhance HomePage with new loan details, images, and apply buttons for car, business, and house loans

--- frontend/app/components/home.tsx
+++ frontend/app/components/home.tsx
@@ -35,9 +35,49 @@ export default function HomePage() {
           />
         </div>
         <div className={styles["loans"]}>
-          <h3>Wanna have your dream car?</h3>
-          <h4>We can help you with our <span>CAR LOAN</span>.</h4>
-          <p>with as low as <span>X% interest</span>, you can now drive the car you really need.</p>
+          <div className={styles["car-details"]}>
+            <h3>Wanna have your dream car?</h3>
+            <h4>We can help you with our <span>CAR LOAN</span>.</h4>
+            <p>with as low as <span>X% interest</span>, you can now drive the car you really need.</p>
+            <button className={styles["loans-button"]}>Apply now</button>
+          </div>
+          <Image
+            src="/images/Car Loan Services.png"
+            alt="Car Loan Image"
+            className={`${styles["loans-image"]} ${styles["car"]}`}
+            width={600}
+            height={600}
+          />
+        </div>
+        <div className={`${styles["loans"]} ${styles["business-card"]}`}>
+          <Image
+            src="/images/work.png"
+            alt="Work Image"
+            className={`${styles["loans-image"]} ${styles["business"]}`}
+            width={600}
+            height={400}
+          />
+          <div className={styles["loan-details"]}>
+            <h3>Wanna expand your business?</h3>
+            <h4>We can help you with our <span>BUSINESS LOAN</span>.</h4>
+            <p>with as low as <span>X% interest</span>, you can now expand your <br /> business and earn more money than you used to.</p>
+            <button className={styles["loans-button"]}>Apply now</button>
+          </div>
+        </div>
+        <div className={`${styles["loans"]} ${styles["house-card"]}`}>
+          <div className={styles["loan-details"]}>
+            <h3>Wanna have a HOME that you can proudly call your own?</h3>
+            <h4>We can help you with our <span>HOUSE LOAN</span>.</h4>
+            <p>with as low as <span>X% interest</span>, you can now buy your <br /> own home that fits your needs.</p>
+            <button className={styles["loans-button"]}>Apply now</button>
+          </div>
+          <Image
+            src="/images/house_loan.png"
+            alt="Work Image"
+            className={`${styles["loans-image"]} ${styles["house"]}`}
+            width={600}
+            height={400}
+          />
         </div>
       </div>
     </main>
