fb4a279fb226947dea23b499abe623a7331db694
fideljotojot <fideljotojot@gmail.com>
2026-04-11 22:27:38 +0800

    Enhance LoansPage layout with loan cards and styling; add new CSS for layout structure

--- frontend/app/components/pages/loans.tsx
+++ frontend/app/components/pages/loans.tsx
@@ -1,7 +1,33 @@
+import styles from "../../style/loans.module.css"
+
 export default function LoansPage() {
+    const loanCards = [];
+
+    const loanInfo = [
+        {title: "Car Loan", description: "thiscription1"},
+        {title: "Business Loan", description: "thiscription2"},
+        {title: "House Loan", description: "thiscription3"},
+    ]
+
+    for (let i = 0; i < loanInfo.length; i++) {
+        loanCards.push(
+            <div className="data-card" key={i}>
+                <h3 className={styles["title"]}>{loanInfo[i].title}</h3>
+                <p>{loanInfo[i].description}</p>
+            </div>
+        );
+    }
+
     return (
-        <main>
-            <h1>Hi</h1>
+        <main className="wrapper">
+            <div className={styles["top-div"]}>
+                <h1 className="headline">
+                    LOANS OVERVIEW
+                </h1>
+                <div className={styles["loan-container"]}>
+                    {loanCards}
+                </div>
+            </div>
         </main>
     )
 }
